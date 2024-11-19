71b65473ee1e7f56fdc0235bf7ce8744703c0bf8
coreoceantesting <coreoceantesting@gmail.com>
2024-11-19 16:17:21 +0530

    change in month on mobile view

--- app/Http/Controllers/Employee/HomeController.php
+++ app/Http/Controllers/Employee/HomeController.php
@@ -23,40 +23,40 @@ class HomeController extends Controller
         $fromDate = '';
         $toDate = '';
 
-        if($request->month && $request->year)
-        {
-            $settings = Setting::getValues( auth()->user()->tenant_id )->pluck('value', 'key');
-            $fromDate = Carbon::parse($request->year.'-'.$request->month.'-'.$settings['PAYROLL_DATE']);
-            $toDate = clone($fromDate);
+        if ($request->month && $request->year) {
+            $settings = Setting::getValues(auth()->user()->tenant_id)->pluck('value', 'key');
+            $fromDate = Carbon::parse($request->year . '-' . $request->month . '-' . $settings['PAYROLL_DATE']);
+            $toDate = clone ($fromDate);
             $fromDate = (string) $fromDate->subMonth()->toDateString();
             $toDate = (string) $toDate->subDay()->toDateString();
-        }
-        else
-        {
-            $settings = Setting::getValues( auth()->user()->tenant_id )->pluck('value', 'key');
-            $fromDate = Carbon::parse( Carbon::today()->format('Y-m-') .$settings['PAYROLL_DATE']);
-            $toDate = clone($fromDate);
-            $fromDate = (string) $fromDate->subMonth()->toDateString();
-            $toDate = (string) $toDate->subDay()->toDateString();
+        } else {
+            $settings = Setting::getValues(auth()->user()->tenant_id)->pluck('value', 'key');
+            $fromDate = Carbon::parse(Carbon::today()->format('Y-m-') . $settings['PAYROLL_DATE']);
+            $toDate = clone ($fromDate);
+
+            if (date('d') >= 16 && date('d') <= 31) {
+                $fromDate = (string) $fromDate->toDateString();
+                $toDate = (string) $toDate->addMonth(1)->subDay()->toDateString();
+            } else {
+                $fromDate = (string) $fromDate->subMonth()->toDateString();
+                $toDate = (string) $toDate->subDay()->toDateString();
+            }
         }
 
         $authUser = Auth::user();
         $punches = Punch::query()
-                    ->select('id', 'emp_code', 'check_in', 'check_out', 'punch_date', 'is_latemark', 'type', 'leave_type_id', 'is_paid', 'duration', 'punch_by')
-                    ->whereDate('punch_date', '>=', $fromDate)->whereDate('punch_date', '<=', $toDate)
-                    ->where('emp_code', $authUser->emp_code)
-                    ->where('punch_by', '0')
-                    ->orderByDesc('punch_date')
-                    ->get();
+            ->select('id', 'emp_code', 'check_in', 'check_out', 'punch_date', 'is_latemark', 'type', 'leave_type_id', 'is_paid', 'duration', 'punch_by')
+            ->whereDate('punch_date', '>=', $fromDate)->whereDate('punch_date', '<=', $toDate)
+            ->where('emp_code', $authUser->emp_code)
+            ->where('punch_by', '0')
+            ->orderByDesc('punch_date')
+            ->get();
 
-        $todaysData = Punch::whereDate('punch_date', Carbon::today()->toDateString())->where('emp_code',$authUser->emp_code)->where('punch_by', '0')->select('check_in', 'check_out', 'duration')->first();
-        if($todaysData)
-        {
+        $todaysData = Punch::whereDate('punch_date', Carbon::today()->toDateString())->where('emp_code', $authUser->emp_code)->where('punch_by', '0')->select('check_in', 'check_out', 'duration')->first();
+        if ($todaysData) {
             $data['todays_in_time'] = $todaysData->check_in ? Carbon::parse($todaysData->check_in)->format('h:i A') : '--';
-            $data['todays_out_time'] = $todaysData->check_out ? ( Carbon::parse($todaysData->check_out)->gt(Carbon::parse($todaysData->check_in)->addMinutes(5)) ? Carbon::parse($todaysData->check_out)->format('h:i A') : '--' ) : '--';
-        }
-        else
-        {
+            $data['todays_out_time'] = $todaysData->check_out ? (Carbon::parse($todaysData->check_out)->gt(Carbon::parse($todaysData->check_in)->addMinutes(5)) ? Carbon::parse($todaysData->check_out)->format('h:i A') : '--') : '--';
+        } else {
             $data['todays_in_time'] = '--';
             $data['todays_out_time'] = '--';
         }
@@ -66,37 +66,34 @@ class HomeController extends Controller
         $calendarData = [];
         $data['total_present'] = 0;
         $data['total_absent'] = 0;
-            foreach ($punches as $punch)
-            {
-                if($punch->check_in)
-                {
-                    array_push($calendarData, array(
-                        'date'=> Carbon::parse($punch->punch_date)->format('d-m-Y'),
-                        'check_in'=> Carbon::parse($punch->check_in)->format('h:i A'),
-                        // 'check_out'=> $punch->check_out ? Carbon::parse($punch->check_out)->format('h:i A') : '----',
-                        'check_out'=> $punch->check_out ? ( Carbon::parse($punch->check_out)->gt(Carbon::parse($punch->check_in)->addMinutes(5)) ? Carbon::parse($punch->check_out)->format('h:i A') : '----' ) : '----',
-                    ));
-                    $actualPresent++;
-                }
-                else
-                    $absendDays++;
+        foreach ($punches as $punch) {
+            if ($punch->check_in) {
+                array_push($calendarData, array(
+                    'date' => Carbon::parse($punch->punch_date)->format('d-m-Y'),
+                    'check_in' => Carbon::parse($punch->check_in)->format('h:i A'),
+                    // 'check_out'=> $punch->check_out ? Carbon::parse($punch->check_out)->format('h:i A') : '----',
+                    'check_out' => $punch->check_out ? (Carbon::parse($punch->check_out)->gt(Carbon::parse($punch->check_in)->addMinutes(5)) ? Carbon::parse($punch->check_out)->format('h:i A') : '----') : '----',
+                ));
+                $actualPresent++;
+            } else
+                $absendDays++;
 
-                $data['total_present'] = $actualPresent;
-                $data['total_absent'] = $absendDays;
-            }
+            $data['total_present'] = $actualPresent;
+            $data['total_absent'] = $absendDays;
+        }
         $data['total_leave'] = $punches->where('punch_by', 2)->count();
 
         $data['attendance'] = $calendarData;
 
 
-        return view('employee.home')->with(['data'=> $data]);
+        return view('employee.home')->with(['data' => $data]);
     }
 
     public function logout()
     {
         auth()->guard('employee')->logout();
 
-        return redirect()->route('login', ['device_type'=> 'mobile']);
+        return redirect()->route('login', ['device_type' => 'mobile']);
     }
 
     public function privacyPolicy()
@@ -117,36 +114,27 @@ class HomeController extends Controller
             'confirm_password' => 'required|same:password',
         ]);
 
-        if ($validator->passes())
-        {
+        if ($validator->passes()) {
             $old_password = $request->old_password;
             $password = $request->password;
 
-            try
-            {
+            try {
                 $user = DB::table('app_users')->where('id', $request->user()->id)->first();
 
-                if( Hash::check($old_password, $user->password) )
-                {
-                    DB::table('app_users')->where('id', $request->user()->id)->update(['password'=> Hash::make($password)]);
+                if (Hash::check($old_password, $user->password)) {
+                    DB::table('app_users')->where('id', $request->user()->id)->update(['password' => Hash::make($password)]);
 
-                    return response()->json(['success'=> 'Password changed successfully!']);
+                    return response()->json(['success' => 'Password changed successfully!']);
+                } else {
+                    return response()->json(['error2' => 'Old password does not match']);
                 }
-                else
-                {
-                    return response()->json(['error2'=> 'Old password does not match']);
-                }
-            }
-            catch(\Exception $e)
-            {
+            } catch (\Exception $e) {
                 DB::rollBack();
-                Log::info("password change error:". $e);
-                return response()->json(['error2'=> 'Something went wrong while changing your password!']);
+                Log::info("password change error:" . $e);
+                return response()->json(['error2' => 'Something went wrong while changing your password!']);
             }
-        }
-        else
-        {
-            return response()->json(['error'=>$validator->errors()]);
+        } else {
+            return response()->json(['error' => $validator->errors()]);
         }
     }
 
@@ -156,6 +144,6 @@ class HomeController extends Controller
 
         auth()->guard('employee')->logout();
 
-        return response()->json(['success'=> 'Account deleted successfully!']);
+        return response()->json(['success' => 'Account deleted successfully!']);
     }
 }
